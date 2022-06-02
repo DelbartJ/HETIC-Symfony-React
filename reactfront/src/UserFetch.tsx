ad7589aa629d1652e5297a586370635a1fb5e36f
DelbartJ <jeremu.delbart@gmail.com>
2022-06-02 19:31:41 +0200

    fetch

--- reactfront/src/UserFetch.tsx
+++ reactfront/src/UserFetch.tsx
@@ -1,1 +1,19 @@
 import { useEffect, useState } from "react";
+
+export default function fetchPost() {
+    const [fetchedData, setFetchedData] = useState<object>({})
+
+    useEffect(() => {
+        fetch('/manifest.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setFetchedData(data)
+            })
+
+    }, [])
+
+    return (
+        <div/>
+    )
+}
